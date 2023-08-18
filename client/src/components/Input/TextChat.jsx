27ca9b20bc43e223158b6406a3d331ae4332f1e0
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-08-18 16:08:42 +0530

    Refactor devcontainer.json and TextChat.jsx
    - Refactored the devcontainer.json by organizing the code and removing unnecessary whitespace.
    - Refactored the background styling in TextChat.jsx by replacing specific color codes with variables for better readability and maintainability.

--- client/src/components/Input/TextChat.jsx
+++ client/src/components/Input/TextChat.jsx
@@ -130,10 +130,10 @@ export default function TextChat({ isSearchView = false }) {
         className="no-gradient-sm fixed bottom-0 left-0 w-full pt-6 sm:bg-gradient-to-b md:absolute"
         style={{
           background: `linear-gradient(to bottom, 
-                ${isDark ? '#0a3b5c00' : 'rgba(255, 255, 255, 0)'}, 
-                ${isDark ? '#0a3b5c14' : 'rgba(255, 255, 255, 0.08)'}, 
-                ${isDark ? '#0a3b5c61' : 'rgba(255, 255, 255, 0.38)'}, 
-                ${isDark ? '#0a3b5c' : 'rgba(255, 255, 255, 1)'}, 
+                ${isDark ? 'rgba(10, 59, 92, 0)' : 'rgba(255, 255, 255, 0)'}, 
+                ${isDark ? 'rgba(10, 59, 92, 0.08)' : 'rgba(255, 255, 255, 0.08)'}, 
+                ${isDark ? 'rgba(10, 59, 92, 0.38)' : 'rgba(255, 255, 255, 0.38)'}, 
+                ${isDark ? 'rgba(10, 59, 92, 1)' : 'rgba(255, 255, 255, 1)'}, 
                 ${isDark ? '#0a3b5c' : '#ffffff'})`,
         }}
       >
